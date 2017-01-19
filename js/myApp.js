25f764737ec27a99137a270f02cc25399353b96e
Greg Duncan <gregdd@outlook.com>
2017-01-19 22:22:15 +1000

    Set up object to hold all data (shoppingList)

---
+++ js/myApp.js
@@ -0,0 +1,30 @@
+
+var shoppingList = {
+    items: [],
+    addItem: function (itemName, price){
+        this.items.push({
+            itemName: itemName,
+            price: price,
+            buy: true
+        });
+    },
+    showList: function (){
+        var toShow = [];
+        var total = 0;
+        this.items.forEach(function (item) {
+            if (item.buy){
+                toShow.push(item.itemName);
+                total += item.price;
+            }
+        });
+        console.log(toShow, 'total: ', total);
+    }
+};
+
+var handler = {
+
+};
+
+var view = {
+
+};
