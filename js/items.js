b0d123f054e4a37e9fcebbab762d743faba2d0f1
Greg Duncan <gregdd@outlook.com>
2017-01-26 11:42:25 +1000

    removed some console outputs

--- js/items.js
+++ js/items.js
@@ -7,41 +7,54 @@ var catalogue = {
                 itemName: 'apples',
                 price: 3.99,
                 buy: true,
-                category: ['produce']
+                category: ['produce'],
+                store: 'Fruit_Market'
             },
             {
                 itemName: 'oranges',
                 price: 2.99,
                 buy: true,
-                category: ['produce']
+                category: ['produce'],
+                store: 'Fruit_Market'
             },
             {
                 itemName: 'tomatoes',
                 price: 1.99,
                 buy: true,
-                category: []
+                category: [],
+                store: 'Fruit_Market'
             }
         ],
     },
-    meatAndDairy: {
+    fresh: {
         items: [
             {
                 itemName: 'milk',
                 price: 2.79,
                 buy: true,
-                category: ['meatAndDairy']
+                category: ['fresh'],
+                store: 'Coles'
             },
             {
                 itemName: 'cheese',
                 price: 6.99,
                 buy: true,
-                category: ['meatAndDairy']
+                category: ['fresh'],
+                store: 'Aldi'
             },
             {
                 itemName: 'chicken_legs',
                 price: 2.99,
                 buy: true,
-                category: ['meatAndDairy']
+                category: ['fresh'],
+                store: 'Aldi'
+            },
+            {
+                itemName: 'rye_bread',
+                price: 3.99,
+                buy: true,
+                category: ['fresh'],
+                store: 'Coles'
             }
         ]
     },
@@ -51,20 +64,57 @@ var catalogue = {
                 itemName: 'dish_soap',
                 price: 2.12,
                 buy: true,
-                category: ['cleaning']
+                category: ['cleaning'],
+                store: 'Aldi'
             },
             {
                 itemName: 'face_wash',
                 price: 2.99,
                 buy: true,
-                category: ['cleaning']
+                category: ['cleaning'],
+                store: 'Aldi'
             },
             {
                 itemName: 'tissues',
                 price: 1.89,
                 buy: true,
-                category: ['cleaning']
+                category: ['cleaning'],
+                store: 'Aldi'
+            }
+        ]
+    },
+    beverages: {
+        items: [
+            {
+                itemName: 'coffee',
+                price: 12,
+                buy: true,
+                category: ['beverages'],
+                store: 'Coles'
+            },
+            {
+                itemName: 'tea',
+                price: 1.90,
+                buy: true,
+                category: ['beverages'],
+                store: 'Aldi'
+            },
+            {
+                itemName: 'soft_drink',
+                price: 1.2,
+                buy: true,
+                category: ['beverages'],
+                store: 'Coles'
             }
         ]
     }
 };
+/*
+            {
+                itemName: '',
+                price: ,
+                buy: true,
+                category: [''],
+                store: ''
+            }
+*/
--- js/myApp.js
+++ js/myApp.js
@@ -1,19 +1,10 @@
 
 var shoppingList = {
+    shops: ['Adli', 'Coles', 'Fruit_Market', 'other' ],
     items: [],
-    addItemManual: function (itemName, price){
-        this.items.push({
-            itemName: itemName,
-            price: price,
-            buy: true,
-            category: []
-        });
-        this.showList();
-    },
     loadList: function(list){
         console.log('list ',list);
         var categorys = Object.keys(catalogue);
-        //debugger;
         categorys.forEach(function(category){
             catalogue[category].items.forEach(function(item){
                 shoppingList.items.push(item);
@@ -30,16 +21,13 @@ var shoppingList = {
         this.showList();
     },
     showList: function (){
-        var toShow = [];
         var total = 0;
         this.items.forEach(function (item) {
             if (item.buy){
-                toShow.push(item.itemName);
                 total += item.price;
                 total = Math.round(total * 100) / 100; // make sure rounds properly
             }
         });
-        console.log(toShow, 'total: ', total);
         view.displayItems();
     }
 };
@@ -47,8 +35,6 @@ var shoppingList = {
 var handlers = {
     loadList: function(){
         console.log('catalogue ', catalogue);
-        //var categorys = Object.keys(catalogue);
-        //console.log(categorys);
         shoppingList.loadList(catalogue);
 
     } 
@@ -73,3 +59,15 @@ var view = {
     }
 };
 
+
+/* 
+    addItemManual: function (itemName, price){
+        this.items.push({
+            itemName: itemName,
+            price: price,
+            buy: true,
+            category: []
+        });
+        this.showList();
+    },
+*/
